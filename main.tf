a2a63988dab42bc50dcf6527e5c0dfa794ff98e4
SanjeevKrishnaR <sanjeev.krishna.menon@gmail.com>
2025-06-08 11:45:32 +0000

    m

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "practice_rg" {
-  name     = var.rg_name
-  location = var.rg_location
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "rg_name" {
-  type    = string
-  default = "practice-terraform-rg"
-}
-
-variable "rg_location" {
-  type    = string
-  default = "East US"
-}
